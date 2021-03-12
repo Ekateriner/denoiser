520fed7b0e53cb61b4e0f616fa057a1e5b7f62ca
Ekateriner <yushina.ee@phystech.edu>
2021-03-13 01:01:23 +0300

    dataset settings

---
+++ make_valentini.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# authors: adiyoss and adefossez
+
+path=egs/valentiny/tr
+if test ! -e $path ; then
+    mkdir -p $path
+fi
+python3 -m denoiser.audio $1/noisy_trainset_28spk_wav > $path/noisy.json
+python3 -m denoiser.audio $1/clean_trainset_28spk_wav > $path/clean.json
+
+path=egs/valentiny/tt
+if test ! -e $path ; then
+    mkdir -p $path
+fi
+python3 -m denoiser.audio $1/noisy_testset_wav > $path/noisy.json
+python3 -m denoiser.audio $1/clean_testset_wav > $path/clean.json
