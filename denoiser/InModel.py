812fb4c966fe6736854b95d8af89656b4e18f7a0
Ekateriner <yushina.ee@phystech.edu>
2021-05-15 14:35:25 +0300

    relu to elu in 2ch

--- denoiser/InModel.py
+++ denoiser/InModel.py
@@ -31,7 +31,6 @@ class InEnhancer_2ch(nn.Module):
             - floor (float): stability flooring when normalizing.
 
         """
-
     @capture_init
     def __init__(self,
                  chin=2,
@@ -64,8 +63,35 @@ class InEnhancer_2ch(nn.Module):
         self.resample = resample
         self.normalize = normalize
 
-        self.denoiser = Demucs(chin, chout, hidden, depth, kernel_size, stride, causal,
-                                     resample, growth, max_hidden, normalize, glu, rescale, floor)
+        self.encoder = nn.ModuleList()
+        self.decoder = nn.ModuleList()
+        activation = nn.GLU(1) if glu else nn.ReLU()
+        ch_scale = 2 if glu else 1
+
+        for index in range(depth):
+            encode = []
+            encode += [
+                nn.Conv1d(chin, hidden, kernel_size, stride),
+                nn.ELU(),
+                nn.Conv1d(hidden, hidden * ch_scale, 1), activation,
+            ]
+            self.encoder.append(nn.Sequential(*encode))
+
+            decode = []
+            decode += [
+                nn.Conv1d(hidden, ch_scale * hidden, 1), activation,
+                nn.ConvTranspose1d(hidden, chout, kernel_size, stride),
+            ]
+            if index > 0:
+                decode.append(nn.ReLU())
+            self.decoder.insert(0, nn.Sequential(*decode))
+            chout = hidden
+            chin = hidden
+            hidden = min(int(growth * hidden), max_hidden)
+
+        self.lstm = BLSTM(chin, bi=not causal)
+        if rescale:
+            rescale_module(self, reference=rescale)
 
     def valid_length(self, length):
         """
@@ -90,10 +116,43 @@ class InEnhancer_2ch(nn.Module):
         return self.stride ** self.depth // self.resample
 
     def forward(self, mix):
-        input = th.cat([mix, -mix], dim=1)
+        mix = th.cat([mix, -mix], dim=1)
+        if mix.dim() == 2:
+            mix = mix.unsqueeze(1)
 
-        result = self.denoiser(input)
-        return result
+        if self.normalize:
+            mono = mix.mean(dim=1, keepdim=True)
+            std = mono.std(dim=-1, keepdim=True)
+            mix = mix / (self.floor + std)
+        else:
+            std = 1
+        length = mix.shape[-1]
+        x = mix
+        x = F.pad(x, (0, self.valid_length(length) - length))
+        if self.resample == 2:
+            x = upsample2(x)
+        elif self.resample == 4:
+            x = upsample2(x)
+            x = upsample2(x)
+        skips = []
+        for encode in self.encoder:
+            x = encode(x)
+            skips.append(x)
+        x = x.permute(2, 0, 1)
+        x, _ = self.lstm(x)
+        x = x.permute(1, 2, 0)
+        for decode in self.decoder:
+            skip = skips.pop(-1)
+            x = x + skip[..., :x.shape[-1]]
+            x = decode(x)
+        if self.resample == 2:
+            x = downsample2(x)
+        elif self.resample == 4:
+            x = downsample2(x)
+            x = downsample2(x)
+
+        x = x[..., :length]
+        return std * x
 
 class InEnhancer_rep(nn.Module):
     """
